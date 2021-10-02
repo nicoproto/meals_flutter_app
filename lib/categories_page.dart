59bbc9abcaf466ebd0d533c973caf5a533acf224
Nico Proto <nicolasproto@gmail.com>
2021-10-02 16:40:24 +0200

    Styling & Theming

--- lib/categories_page.dart
+++ lib/categories_page.dart
@@ -10,6 +10,7 @@ class CategoriesScreen extends StatelessWidget {
         title: const Text("Super Meals"),
       ),
       body: GridView(
+          padding: const EdgeInsets.all(25),
           children: DUMMY_CATEGORIES
               .map((e) => CategoryItem(e.title, e.color))
               .toList(),
